6e02578c82770e16672553429a54545b6ae80385
Saivrem <saivrem@gmail.com>
2020-09-07 12:29:32 +0300

    initial commit

---
+++ src/main/java/in/ua/icetools/icedata/IcedataApplication.java
@@ -0,0 +1,13 @@
+package in.ua.icetools.icedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcedataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcedataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/ua/icetools/icedata/IcedataApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ua.icetools.icedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcedataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
