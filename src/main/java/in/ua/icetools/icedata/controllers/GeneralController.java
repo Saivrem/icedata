6e9c9e82f0a8b5e8d7a3f0e78d58363442416e39
Denis Sheviakov <saivrem@gmail.com>
2020-12-17 18:14:47 +0200

    I've changed something, some time ago.

--- src/main/java/in/ua/icetools/icedata/controllers/GeneralController.java
+++ src/main/java/in/ua/icetools/icedata/controllers/GeneralController.java
@@ -1,8 +1,11 @@
 package in.ua.icetools.icedata.controllers;
 
+import in.ua.icetools.icedata.processors.LanguageProcessor;
 import in.ua.icetools.icedata.processors.SupplierMappingProcessor;
 import in.ua.icetools.icedata.processors.SupplierProcessor;
 import in.ua.icetools.icedata.processors.Utils;
+import in.ua.icetools.icedata.resources.LanguageNameRepository;
+import in.ua.icetools.icedata.resources.LanguageRepository;
 import in.ua.icetools.icedata.resources.SupplierMappingRepository;
 import in.ua.icetools.icedata.resources.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +26,10 @@ public class GeneralController {
     SupplierRepository supplierRepository;
     @Autowired
     SupplierMappingRepository supplierMappingRepository;
+    @Autowired
+    LanguageRepository languageRepository;
+    @Autowired
+    LanguageNameRepository languageNameRepository;
 
     @PostMapping("/init")
     public ResponseEntity<String> init(@Valid @RequestBody Properties properties) {
@@ -37,6 +44,8 @@ public class GeneralController {
             supplierRepository.saveAll(SupplierProcessor.process(false, null));
             supplierMappingRepository.truncate();
             supplierMappingRepository.saveAll(SupplierMappingProcessor.process());
+            languageRepository.saveAll(LanguageProcessor.process(false, null));
+            languageNameRepository.saveAll(LanguageProcessor.getNamesList());
         } catch (Exception e) {
             response = e.getMessage();
         }
